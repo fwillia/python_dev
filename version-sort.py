716e0c3d98ab04a35ca7a4003a949d8413c15a81
Fredrick Williams <fwilliams@regis.edu>
2022-03-24 23:29:30 -0500

    Initial check-in of version-sort.py

---
+++ version-sort.py
@@ -0,0 +1,9 @@
+print("This line will be printed")
+
+numList = [1.0, 1.2, 1.1, 1.3]
+strList = ['apple', 'cherry', 'banana', 'date']
+alphaList=['1.0', '3.0', '2.0', '4.0', '2.0.beta', '2.0.alpha']
+
+print(sorted(numList))
+print(sorted(strList))
+print(sorted(alphaList))
